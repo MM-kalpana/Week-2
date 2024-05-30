6b54741956e2a52e2509491430ecb105ba025ffa
Kalpanabharath <stonepanakeerthu@gmail.com>
2024-05-30 10:50:54 +0530

    FIx

--- script.js
+++ script.js
@@ -44,6 +44,10 @@ document.addEventListener('DOMContentLoaded', function() {
     // bodyEl.style.backgroundImage = 'linear-gradient(to right, rgb(245, 176, 115),rgb(255, 222, 194))'
  
 });
+
+
+
+
  function callfun(){
   leftside.innerHTML=""
   leftside.innerHTML=`
@@ -59,21 +63,18 @@ document.addEventListener('DOMContentLoaded', function() {
           <img src="./image/you tube.png" alt="YouTube">
         </div>
     `
-    bodyEl.style.backgroundImage = 'linear-gradient(to right, rgb(245, 176, 115),rgb(255, 222, 194))'
+    // bodyEl.style.backgroundImage = 'linear-gradient(to right, rgb(245, 176, 115),rgb(255, 222, 194))'
     console.log("working")
 
+    var h2Element = document.querySelector('#leftheading h2');
+    console.log(h2Element)
+    
  }
-
-  
- 
-
- let watchpinkEl=document.getElementById("watchpink");
+let watchpinkEl=document.getElementById("watchpink");
  let watchpinkfun= new IntersectionObserver(callfun)
  watchpinkfun.observe(watchpinkEl);
 
-
-
- function blacksilver(){
+function blacksilver(){
   leftside.innerHTML=""
   leftside.innerHTML=`
     <div id="leftheading">
@@ -89,7 +90,9 @@ document.addEventListener('DOMContentLoaded', function() {
         </div>
     `
   console.log("working black")
-  bodyEl.style.backgroundImage = 'linear-gradient(to right, rgb(177, 181, 181),rgb(224, 224, 224))'
+  // bodyEl.style.backgroundImage = 'linear-gradient(to right, rgb(177, 181, 181),rgb(224, 224, 224))'
+  
+  
 
  }
 let blacksilverEl=document.getElementById("blacksilver");
@@ -104,7 +107,7 @@ function greensilver(){
   leftside.innerHTML=""
   leftside.innerHTML=`
     <div id="leftheading">
-          <h2>Elegant Timepieces</h2>
+          <h2 >Elegant Timepieces</h2>
           <h3>Choose Luxury<span>Choose Us</span></h3>
       </div>
         <p>Discover the Perfect Watch for Every Occasion and Elevate Your Style with Timeless Elegance and Precision Craftsmanship - watch</p>
@@ -115,8 +118,11 @@ function greensilver(){
           <img src="./image/you tube.png" alt="YouTube">
         </div>
     `
-    bodyEl.style.backgroundImage = 'linear-gradient(to right, rgb(59, 173, 99),rgb(116, 227, 153))'
+  // bodyEl.style.backgroundImage = 'linear-gradient(to right, rgb(59, 173, 99),rgb(116, 227, 153))'
   console.log("working green")
+  
+
+
 
  }
 let greensilverEl=document.getElementById("greensilver");
@@ -128,7 +134,7 @@ function redsilver(){
   leftside.innerHTML=""
   leftside.innerHTML=`
     <div id="leftheading">
-          <h2>Refined Timepieces</h2>
+          <h2 >Refined Timepieces</h2>
           <h3>Choose Luxury<span>Choose Us</span></h3>
       </div>
         <p>Explore the Ideal Timepiece for Any Moment and Enhance Your Style with Timeless Sophistication and Impeccable Craftsmanship - timepiece</p>
@@ -139,11 +145,49 @@ function redsilver(){
           <img src="./image/you tube.png" alt="YouTube">
         </div>
     `
-    bodyEl.style.backgroundImage = 'linear-gradient(to right, rgb(245, 98, 93),rgb(255, 168, 148))'
+    // bodyEl.style.backgroundImage = 'linear-gradient(to right, rgb(245, 98, 93),rgb(255, 168, 148))'
   console.log("working red")
 
- }
+
+}
 let redsilverEl=document.getElementById("redsilver");
 let redsilverfun= new IntersectionObserver(redsilver);
 redsilverfun.observe(redsilverEl);
+
+///fix
+
+const callback = (entries) => {
+  entries.forEach(entry => {
+      if (entry.isIntersecting) {
+          if (entry.target.id === "watchpink") {
+              console.log("finalwork");
+              bodyEl.style.backgroundImage = 'linear-gradient(to right, rgb(245, 176, 115),rgb(255, 222, 194))'
+          }else if(entry.target.id === "blacksilver"){
+            bodyEl.style.backgroundImage = 'linear-gradient(to right, rgb(177, 181, 181),rgb(224, 224, 224))'
+          }else if(entry.target.id === "greensilver"){
+            bodyEl.style.backgroundImage = 'linear-gradient(to right, rgb(59, 173, 99),rgb(116, 227, 153))'
+          } else{
+            bodyEl.style.backgroundImage = 'linear-gradient(to right, rgb(245, 98, 93),rgb(255, 168, 148))'
+          }
+      }
+  });
+};
+
+// Create an intersection observer
+const observer = new IntersectionObserver(callback, {
+  root: null, // Use the viewport as the root
+  threshold: 0.5 // Adjust the threshold as needed
+});
+
+// Select all images in the splide__slide class and observe them
+const images = document.querySelectorAll('.splide__slide img');
+images.forEach(img => {
+  observer.observe(img);
+});
+
+
+
+
+
+
 
