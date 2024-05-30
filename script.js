c871e1014360593d9e5cd87583ef76913daacccf
Kalpanabharath <stonepanakeerthu@gmail.com>
2024-05-30 06:25:43 +0530

    FIX:media

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ let headerdata=[
 
     }
 ]
+let bodyEl=document.getElementById("body");
 
 let leftside=document.getElementById('leftside');
 const header = document.getElementById('header');
@@ -27,23 +28,22 @@ document.addEventListener('DOMContentLoaded', function() {
         </div>
       </nav>
     `;
-    leftside.innerHTML=`
-    <div id="leftheading">
-          <h2>Exquisite Watches</h2>
-          <h3>Gold Luxury<span>Choose Us</span></h3>
-      </div>
-        <p>Discover the Perfect Watch for Every Occasion and Elevate Your Style with Timeless Elegance and Precision Craftsmanship - watch</p>
-        <h1>$499.00</h1>
-        <div id="icondiv">
-          <img src="./image/fb.png" alt="Facebook">
-          <img src="./image/twitter.png" alt="Twitter">
-          <img src="./image/you tube.png" alt="YouTube">
-        </div>
-    `
+    // leftside.innerHTML=`
+    // <div id="leftheading">
+    //       <h2>Exquisite Watches</h2>
+    //       <h3>Gold Luxury<span>Choose Us</span></h3>
+    //   </div>
+    //     <p>Discover the Perfect Watch for Every Occasion and Elevate Your Style with Timeless Elegance and Precision Craftsmanship - watch</p>
+    //     <h1>$499.00</h1>
+    //     <div id="icondiv">
+    //       <img src="./image/fb.png" alt="Facebook">
+    //       <img src="./image/twitter.png" alt="Twitter">
+    //       <img src="./image/you tube.png" alt="YouTube">
+    //     </div>
+    // `
+    // bodyEl.style.backgroundImage = 'linear-gradient(to right, rgb(245, 176, 115),rgb(255, 222, 194))'
  
-
-    
-  });
+});
  function callfun(){
   leftside.innerHTML=""
   leftside.innerHTML=`
@@ -59,11 +59,16 @@ document.addEventListener('DOMContentLoaded', function() {
           <img src="./image/you tube.png" alt="YouTube">
         </div>
     `
+    bodyEl.style.backgroundImage = 'linear-gradient(to right, rgb(245, 176, 115),rgb(255, 222, 194))'
     console.log("working")
 
  }
+
+  
+ 
+
  let watchpinkEl=document.getElementById("watchpink");
- let watchpinkfun= new IntersectionObserver(callfun);
+ let watchpinkfun= new IntersectionObserver(callfun)
  watchpinkfun.observe(watchpinkEl);
 
 
@@ -84,10 +89,13 @@ document.addEventListener('DOMContentLoaded', function() {
         </div>
     `
   console.log("working black")
+  bodyEl.style.backgroundImage = 'linear-gradient(to right, rgb(177, 181, 181),rgb(224, 224, 224))'
 
  }
 let blacksilverEl=document.getElementById("blacksilver");
 let blacksilverfun= new IntersectionObserver(blacksilver)
+
+
 blacksilverfun.observe(blacksilverEl)
 
 
@@ -107,9 +115,35 @@ function greensilver(){
           <img src="./image/you tube.png" alt="YouTube">
         </div>
     `
-  console.log("working black")
+    bodyEl.style.backgroundImage = 'linear-gradient(to right, rgb(59, 173, 99),rgb(116, 227, 153))'
+  console.log("working green")
 
  }
 let greensilverEl=document.getElementById("greensilver");
 let greensilverfun= new IntersectionObserver(greensilver)
 greensilverfun.observe(greensilverEl)
+
+
+function redsilver(){
+  leftside.innerHTML=""
+  leftside.innerHTML=`
+    <div id="leftheading">
+          <h2>Refined Timepieces</h2>
+          <h3>Choose Luxury<span>Choose Us</span></h3>
+      </div>
+        <p>Explore the Ideal Timepiece for Any Moment and Enhance Your Style with Timeless Sophistication and Impeccable Craftsmanship - timepiece</p>
+        <h1>$469.00</h1>
+        <div id="icondiv">
+          <img src="./image/fb.png" alt="Facebook">
+          <img src="./image/twitter.png" alt="Twitter">
+          <img src="./image/you tube.png" alt="YouTube">
+        </div>
+    `
+    bodyEl.style.backgroundImage = 'linear-gradient(to right, rgb(245, 98, 93),rgb(255, 168, 148))'
+  console.log("working red")
+
+ }
+let redsilverEl=document.getElementById("redsilver");
+let redsilverfun= new IntersectionObserver(redsilver);
+redsilverfun.observe(redsilverEl);
+
